790ba485e09a78c57473582780b03633b8e6bd6d
ccarter92 <54338971+ccarter92@users.noreply.github.com>
2019-10-03 12:59:08 -0500

    Add files via upload

---
+++ Index.cshtml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Movies.Pages;
+
+
+namespace Movies.Pages
+{
+}
+public class IndexModel : PageModel
+{
+
+    public List <Movies1> Movies { get; set; } = new List <Movies1>();
+    public string MainTitle { get; set; }
+    public string[] Movies1 { get; set; }
+
+    public void OnGet()
+    {
+
+        Movies.Add(new Movies1
+        {
+            MovieId = "j235z",
+            Title = "Booty Call",
+            Director = "Jamiee Foxx",
+            Rating = 87.5
+        });
+        Movies.Add(new Movies1
+        {
+            MovieId = "2hj3",
+            Title = "Black Panther",
+            Director = "Kenyatta Hayes",
+            Rating = 2.3
+        });
+        Movies.Add(new Movies1
+        {
+            MovieId = "234hjke",
+            Title = "Coming To America 2",
+            Director = "Charlie Murphy",
+            Rating = 90.3
+        });
+
+        MainTitle = "Movie Time!";
+        Movies1 = new string[] { "Booty Call", "Black Panther", "Coming To America 2" };
+            
+
+    }
+}
